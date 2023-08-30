eadacb8f95b0287bd41cf78e106f444fc3a0707e
Brian-thee-shovi <brian14198@gmail.com>
2023-08-30 05:47:41 -0700

    measures the size of a binary tree innit

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - this measures the size of binary
+ * @tree: pointer to the root node
+ * Return: size of binary tree
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+}
